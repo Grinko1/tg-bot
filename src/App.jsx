2d65c2c77ee203956af54337b090f90345abb842
Grinko1 <86093873+Grinko1@users.noreply.github.com>
2023-04-05 09:07:32 +0900

    add form

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { useEffect } from 'react';
+import { Route, Routes } from 'react-router';
 import './App.css';
+import Form from './components/Form/Form';
 import Header from './components/Header/Header';
+import ProductList from './components/ProductList/ProductList';
 import { useTelegram } from './hooks/useTelegram';
 
 const tg = window.Telegram.WebApp;
@@ -14,7 +17,11 @@ function App() {
   return (
     <div className="App">
       <Header/>
-      <button onClick={onToggleButton}>toggle</button>
+ 
+      <Routes>
+        <Route index element={<ProductList/>}/>
+        <Route path='form' element={<Form/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/ProductList/ProductList.jsx
@@ -0,0 +1,12 @@
+import './ProductList.css';
+
+const ProductList = () => {
+    return (
+        <div>
+            fgdfg
+            <p>products</p>
+        </div>
+    );
+};
+
+export default ProductList;
